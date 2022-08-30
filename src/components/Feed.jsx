2b4e3aac54ef221e85ca686cebaf95eeee0aa6e5
SubhojeetDas1107 <hbksubhojeetdas1107@gmail.com>
2022-08-30 20:49:10 +0530

    Initial commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Latest tech dev");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          This site is developer by Subhojeet Das
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#0097eb" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/utils/constants.js
@@ -0,0 +1,57 @@
+import CodeIcon from '@mui/icons-material/Code';
+import SourceIcon from '@mui/icons-material/Source';
+import DataArrayIcon from '@mui/icons-material/DataArray';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HtmlIcon from '@mui/icons-material/Html';
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import CloudIcon from '@mui/icons-material/Cloud';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import CloudQueueIcon from '@mui/icons-material/CloudQueue';
+import ApiIcon from '@mui/icons-material/Api';
+import SearchIcon from '@mui/icons-material/Search';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import LanguageIcon from '@mui/icons-material/Language';
+import WebhookIcon from '@mui/icons-material/Webhook';
+
+export const logo = 'https://devtube.sanish.me/images/devtube.png';
+
+export const categories = [
+  { name: 'Dev Tech News', icon: <LogoDevIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Data Structures & Algo', icon: <DataObjectIcon />, },
+  { name: 'FreeCodeCamp', icon: <LocalFireDepartmentIcon/>},
+  { name: 'Python', icon: <DataArrayIcon />, },
+  { name: 'Web Dev', icon: <HtmlIcon />, },
+  { name: 'ReactJS', icon: <JavascriptIcon />, },
+  { name: 'MERN Stack', icon: <TerminalIcon />, },
+  { name: 'AI & ML', icon: <PsychologyIcon />, },
+  { name: 'Data Science', icon: <DeveloperModeIcon />, },
+  { name: 'App Development', icon: <PhoneAndroidIcon />, },
+  { name: 'Cloud Computing', icon: <CloudIcon />, },
+  { name: 'GitHub', icon: <GitHubIcon />, },
+  { name: 'Devops', icon: <LogoDevIcon /> },
+  { name: 'AWS', icon: <CloudQueueIcon />, },
+  { name: 'IoT', icon: <IntegrationInstructionsIcon />, },
+  { name: 'Linux', icon: <TerminalIcon />, },
+  { name: 'API', icon: <ApiIcon />, },
+  { name: 'SEO', icon: <SearchIcon />, },
+  { name: 'Blockchain', icon: <WebhookIcon />, },
+  { name: 'Web3', icon: <LanguageIcon />, },
+  { name: 'Crypto', icon: <CurrencyBitcoinIcon />, },
+  { name: 'Upcoming Coding Compete', icon: <DeveloperModeIcon />, },
+  { name: 'Upcoming Hackathons', icon: <SourceIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
